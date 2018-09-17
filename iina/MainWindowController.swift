04acfdbf76a4214c68095c1a905b686a1e79d5ba
Collider LI <lhc199652@gmail.com>
2018-09-17 23:37:30 +0800

    Refactor event system

--- iina/MainWindowController.swift
+++ iina/MainWindowController.swift
@@ -74,7 +74,7 @@ class MainWindowController: PlayerWindowController {
   override var videoView: VideoView {
     return _videoView
   }
-  
+
   lazy private var _videoView: VideoView = VideoView(frame: window!.contentView!.bounds, player: player)
 
   /** The quick setting sidebar (video, audio, subtitles). */
@@ -1200,7 +1200,7 @@ class MainWindowController: PlayerWindowController {
     }
 
     updateWindowParametersForMPV()
-    
+
     // Exit PIP if necessary
     if pipStatus == .inPIP,
       #available(macOS 10.12, *) {
@@ -1298,7 +1298,7 @@ class MainWindowController: PlayerWindowController {
       return
     }
   }
-  
+
   private func restoreDockSettings() {
     NSApp.presentationOptions.remove(.autoHideMenuBar)
     NSApp.presentationOptions.remove(.autoHideDock)
@@ -1476,6 +1476,8 @@ class MainWindowController: PlayerWindowController {
         }
       }
     }
+
+    player.events.emit(.windowResized, data: window.frame)
   }
 
   // resize framebuffer in videoView after resizing.
@@ -1493,6 +1495,14 @@ class MainWindowController: PlayerWindowController {
   }
 
   // MARK: - Window delegate: Activeness status
+  func windowDidMove(_ notification: Notification) {
+    guard let window = window else { return }
+    player.events.emit(.windowMoved, data: window.frame)
+  }
+
+  func windowDidChangeScreen(_ notification: Notification) {
+    videoView.updateDisplayLink()
+  }
 
   func windowDidBecomeKey(_ notification: Notification) {
     window!.makeFirstResponder(window!)
@@ -1536,7 +1546,7 @@ class MainWindowController: PlayerWindowController {
       player.pause()
     }
   }
-  
+
   func windowDidMiniaturize(_ notification: Notification) {
     if Preference.bool(for: .togglePipByMinimizingWindow) && !isWindowMiniaturizedDueToPip {
       if #available(macOS 10.12, *) {
@@ -2670,6 +2680,8 @@ extension MainWindowController: PIPViewControllerDelegate {
         player.pause()
       }
     }
+
+    player.events.emit(.pipChanged, data: true)
   }
 
   func exitPIP() {
@@ -2681,6 +2693,7 @@ extension MainWindowController: PIPViewControllerDelegate {
       // is chosen in this case. See https://bugs.swift.org/browse/SR-8956.
       pip.dismiss(pipVideo!)
     }
+    player.events.emit(.pipChanged, data: false)
   }
 
   func doneExitingPIP() {
@@ -2712,7 +2725,7 @@ extension MainWindowController: PIPViewControllerDelegate {
     guard let window = window else { return }
     // This is called right before we're about to close the PIP
     pipStatus = .intermediate
-    
+
     // Hide the overlay view preemptively, to prevent any issues where it does
     // not hide in time and ends up covering the video view (which will be added
     // to the window under everything else, including the overlay).
